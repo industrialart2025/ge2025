910d160e4cb6ff2abb4dad61a354d8d7842dbef3
aqizakana <archifish56@gmail.com>
2025-02-09 19:14:19 +0900

    fix 指摘内容を修正

--- search/index.js
+++ search/index.js
@@ -1,13 +1,12 @@
-
+//画像URL_IDを抜粋する関数
 function extractFileId(url) {
     const regex = /\/d\/([^\/]+)/;
     const match = url.match(regex);
     return match ? match[1] : null;
 }
 
-async function getJson() {
-    const filePath = "./search/testList.csv"; // CSVファイルのパス
-
+//csvをjsonに変換する関数
+async function getJson(filePath) {
     try {
         // CSVをJSONに変換
         const response = await fetch(filePath);
@@ -25,58 +24,63 @@ async function getJson() {
     }
 }
 
-
+//jsonからli要素を描画する関数
 function htmlToElement(json) {
     const testTitlesElement = document.querySelector(".test-titles");
 
+    testTitlesElement.innerHTML = ''; // 親要素内の子要素をすべて削除
+
     // リストに追加
-    json.map(test => {
+    json.map(item => {
         const listItem = document.createElement("li");
-        listItem.textContent = `タイトル:${test.title}`; // JSONデータのプロパティに合わせて修正
+        listItem.textContent = `タイトル:${item.title}`; // JSONデータのプロパティに合わせて修正
         testTitlesElement.appendChild(listItem);
 
         const studioItem = document.createElement("li");
-        studioItem.textContent = `スタジオ:${test.studio}`; // JSONデータのプロパティに合わせて修正
-        testTitlesElement.appendChild(studioItem);
+        studioItem.textContent = `スタジオ:${item.studio}`; // JSONデータのプロパティに合わせて修正
+        //testTitlesElement.appendChild(studioItem);
 
         const imgItem = document.createElement("img")
-        const fileId = extractFileId(test["notion_URL\r"]) // JSONデータのプロパティに合わせて修正
+        const fileId = extractFileId(item["notion_URL\r"]) // JSONデータのプロパティに合わせて修正
         imgItem.src = `https://lh3.googleusercontent.com/d/${fileId}`; // 画像のURLを設定
-        testTitlesElement.appendChild(imgItem);
+        //testTitlesElement.appendChild(imgItem);
     });
 }
 
-let selectedKey = "";
+let selectedKey = "studio";
 let currentFilter = null;
 
-function selectChange(selectKeys) {
-    // フィルターボタンのためのコンテナを作成
+//スタジオまたは、ジャンルを選択するボタンを作成する関数
+function selectChangeEither(selectKeys) {
     const filterContainer = document.querySelector('.button-container');
     document.body.appendChild(filterContainer);
 
     const filterItem = ["studio", "genre"]
+    
     filterItem.map(key => {
         const select = document.createElement("button");
         select.textContent = key;
         select.addEventListener("click", () => {
+            //デバッグログ
             selectedKey = key;
             console.log("選択された値:", selectedKey);
+
             updateSecondaryButtons();
         });
         filterContainer.appendChild(select);
     })
+
+    //初期状態ではスタジオを選択
+    selectedKey = "studio"
+    updateSecondaryButtons();
+
 }
 
-function selectChange2(selectList) {
-    let container = document.querySelector('.button-container-secondary');
-    
-    if (!container) {
-        container = document.createElement('div');
-
-        document.body.appendChild(container);
-    } else {
-        container.innerHTML = ''; // 前のボタンをクリア
-    }
+//スタジオ及び、ジャンルの各値を選択するボタンを作成する関数
+function selectChangeDetail(selectList) {
+    const container = document.querySelector('.button-container-secondary');
+   
+    container.innerHTML = ''; // 前のボタンをクリア   
 
     // すべて表示ボタンを追加
     const allButton = document.createElement("button");
@@ -100,7 +104,7 @@ function selectChange2(selectList) {
     });
 }
 
-
+//重複を削除する関数
 function listSet(json, key) {
     if (!key) return [];
     const list = [...new Set(json.map(item => item[key]))].filter(value => value && value.length > 0);
@@ -108,10 +112,11 @@ function listSet(json, key) {
     return list;
 }
 
+//スタジオまたは、ジャンルを選択するボタンを更新する関数
 function updateSecondaryButtons() {
     if (selectedKey) {
         const selectList = listSet(jsonResult, selectedKey);
-        selectChange2(selectList);
+        selectChangeDetail(selectList);
     }
 }
 
@@ -125,8 +130,10 @@ function updateArticleList() {
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-    jsonResult = await getJson();
-    selectChange(Object.keys(jsonResult[0]));
+    const filePath = "./search/reserachList.csv"; // CSVファイルのパス
+    jsonResult = await getJson(filePath);
+    
+    selectChangeEither(Object.keys(jsonResult[0]));
     // 初期表示として全件表示
     updateArticleList(); // 初期表示で全件表示しない
 });
